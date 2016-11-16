35a721e06933c322cbf396d894feb83d08ee7c72
victorsv <victor.psr@gmail.com>
2016-11-15 22:16:34 -0300

    Schema alterado para melhor descrever o banco de dados. A versão anterior, por ser criada pelo phpMyAdmin, não é muito bem aceita pelo MySQL Server.

--- bd.sql
+++ bd.sql
@@ -1,93 +1,41 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: 15-Nov-2016 às 20:46
--- Versão do servidor: 10.1.19-MariaDB
--- PHP Version: 7.0.12
-
+-- MySQL schema para a API de Processos de Software.
+-- Modificada para ter uma descrição mais completa do banco de dados.
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `processodesoftware`
 --
-
 -- --------------------------------------------------------
 
+CREATE SCHEMA IF NOT EXISTS `processodesoftware` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `processodesoftware` ;
+--
+-- DROP TABLE IF EXISTS `users` ;
 --
 -- Estrutura da tabela `users`
 --
-
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `idade` int(11) NOT NULL,
-  `senha` varchar(256) NOT NULL
+  `senha` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
+--
+-- DROP TABLE IF EXISTS `users_info` ;
 --
 -- Estrutura da tabela `users_info`
 --
-
-CREATE TABLE `users_info` (
+CREATE TABLE IF NOT EXISTS `users_info` (
   `id` int(11) NOT NULL,
   `facebook` varchar(100) DEFAULT NULL,
   `whatsapp` bigint(11) DEFAULT NULL,
-  `id_user` int(11) NOT NULL
+  `id_user` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_users_info_user_idx`
+    FOREIGN KEY (`id_user`)
+    REFERENCES `processodesoftware`.`users` (`id`)
+	ON DELETE CASCADE -- Nao existe mais o usuario que referencia esse objeto
+	ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users_info`
---
-ALTER TABLE `users_info`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_users_info_user_idx` (`id_user`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `users_info`
---
-ALTER TABLE `users_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- Constraints for dumped tables
---
-
---
--- Limitadores para a tabela `users_info`
---
-ALTER TABLE `users_info`
-  ADD CONSTRAINT `fk_users_info_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
