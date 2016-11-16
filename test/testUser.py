c3baac6cb67ae30b6e10d67330db9d2fefbe232e
Stefano Momo Loss <momoloss10@gmail.com>
2016-11-16 11:07:50 -0300

    Inicio Classe de Testes

---
+++ test/testUser.py
@@ -0,0 +1,74 @@
+import unittest
+import requests
+import json
+
+class Aux:
+    def getFuncition(self, urlbase, url):
+        response = requests.get(urlbase+url)
+        if response.status_code != 200:
+                raise ApiError('GET/{}'.format(resp.status_code))
+        return response
+
+    def postFuncition(self, urlbase, url, param):
+        response = requests.post(urlbase+url, json=param)
+        if response.status_code != 201:
+                raise ApiError('POST/{}'.format(resp.status_code))
+        return response.status_code
+
+   def deleteFuncition(self, urlbase, url,idd):
+        response = requests.get(urlbase+url+idd)
+        if response.status_code != 200:
+                raise ApiError('DELETE/{}'.format(resp.status_code))
+        return response.status_code
+
+   def 	putFuncition(self, urlbase, url, param):
+        response = requests.get(urlbase+url, json=param)
+        if response.status_code != 200:
+                raise ApiError('put/{}'.format(resp.status_code))
+        return response
+
+
+class FunTest(unittest.TestCase):
+    
+    urlbase = ""
+    aux = Aux()
+
+    def testPost(self):
+        url= "/user"
+        param = { "name":"Teste","email":"teste@teste.com","age":"21","password":"123456"}
+        self.assertEqual(aux.postFuncition(urlbase, url, param), 201)
+        
+        url = "/user/email/teste@teste.com"
+        json = aux.getFuncition(urlbase, url)
+        idd = json['id']
+
+        url = "/user/" 
+        deleteFuncition(urlbase, url,idd)
+
+    def testGetByEmail(self):
+        url= "/user"
+        param = { "name":"Teste","email":"teste@teste.com","age":"21","password":"123456"}
+        aux.postFuncition(urlbase, url, param)
+        
+        url = "/user/email/teste@teste.com"
+        json = aux.getFuncition(urlbase, url)
+        self.assertEqual(json['email'], "teste@teste.com")
+
+        idd = json['id']
+        url = "/user/" 
+        deleteFuncition(urlbase, url,idd)
+
+    def testDelete(self):
+        url= "/user"
+        param = { "name":"Teste","email":"teste@teste.com","age":"21","password":"123456"}      
+        
+        url = "/user/email/teste@teste.com"
+        json = aux.getFuncition(urlbase, url)
+
+        idd = json['id']
+        url = "/user/" 
+        self.assertEqual(deleteFuncition(urlbase, url,idd), 200)
+
+if __name__ == '__main__':
+    unittest.main()
+
