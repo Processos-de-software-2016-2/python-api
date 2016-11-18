ea146122c6a58f59a6947bd0833a8cd6b1266f88
victorsv <victor.psr@gmail.com>
2016-11-18 00:25:15 -0300

    Adicionada as tabelas: skills, user_interests, user_skills, user_match.

--- bd.sql
+++ bd.sql
@@ -36,6 +36,81 @@ CREATE TABLE IF NOT EXISTS `users_info` (
   CONSTRAINT `fk_users_info_user_idx`
     FOREIGN KEY (`id_user`)
     REFERENCES `processodesoftware`.`users` (`id`)
-	ON DELETE CASCADE -- Nao existe mais o usuario que referencia esse objeto
-	ON UPDATE NO ACTION
+  ON DELETE CASCADE -- Nao existe mais o usuario que referencia esse objeto
+  ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `skills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user_interests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_skill` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  CONSTRAINT `fk_ui_user_idx`
+    FOREIGN KEY (`id_user`)
+    REFERENCES `processodesoftware`.`users` (`id`)
+    ON DELETE CASCADE -- Nao existe mais o usuario que referencia esse objeto
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ui_skill_idx`
+    FOREIGN KEY (`id_skill`)
+    REFERENCES `processodesoftware`.`skills` (`id`)
+    ON DELETE CASCADE -- Nao existe mais o skill que o usuario tinha
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user_skills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_skill` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  CONSTRAINT `fk_us_user_idx`
+    FOREIGN KEY (`id_user`)
+    REFERENCES `processodesoftware`.`users` (`id`)
+    ON DELETE CASCADE -- Nao existe mais o usuario que referencia esse objeto
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_us_skill_idx`
+    FOREIGN KEY (`id_skill`)
+    REFERENCES `processodesoftware`.`skills` (`id`)
+    ON DELETE CASCADE -- Nao existe mais o skill que o usuario tinha
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user_match` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user_not` int(11) NOT NULL,
+  `id_user_has` int(11) NOT NULL,
+  `id_skill` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  CONSTRAINT `fk_um_user_not_idx`
+    FOREIGN KEY (`id_user_not`)
+    REFERENCES `processodesoftware`.`users` (`id`)
+    ON DELETE CASCADE -- Nao existe mais o usuario que referencia esse objeto
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_um_user_has_idx`
+    FOREIGN KEY (`id_user_has`)
+    REFERENCES `processodesoftware`.`users` (`id`)
+    ON DELETE CASCADE -- Nao existe mais o usuario que referencia esse objeto
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_um_skill_idx`
+    FOREIGN KEY (`id_skill`)
+    REFERENCES `processodesoftware`.`skills` (`id`)
+    ON DELETE CASCADE -- Nao existe mais o skill que o usuario tinha
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Alterações das tabelas para não quebrar nada.
+ALTER TABLE users
+ADD UNIQUE INDEX `email_UNIQUE` (`email` ASC);
+ALTER TABLE users
+ADD UNIQUE INDEX `id_UNIQUE` (`id` ASC);
+ALTER TABLE users_info
+ADD UNIQUE INDEX `id_UNIQUE` (`id` ASC);
