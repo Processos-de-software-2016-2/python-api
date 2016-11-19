f1d1bb424836fa544758a688766d06095dbb84f9
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2016-11-18 21:23:49 -0300

    Adiciona uploader e downloader de imagens fixes #32

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import gevent
 from gevent import socket
 from controller.users import *
 from controller.users_info import *
+from controller.image import *
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
@@ -21,3 +22,5 @@ app.add_route('/login', Login())
 app.add_route('/infos', UsersInfo())
 app.add_route('/info', UserInfo())
 app.add_route('/info/{id}', UserInfo())
+app.add_route('/picture', Images())
+app.add_route('/picture/{id}', Images())
