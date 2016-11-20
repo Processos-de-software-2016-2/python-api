05e08a8938c8dcf2725395da12a0e87dd3eef87d
danielmanfred <danielmanfred@gmail.com>
2016-11-20 17:52:32 -0300

    Correção de issue (Fixes #45)

--- model/user_interest.py
+++ model/user_interest.py
@@ -2,7 +2,7 @@
 
 class UserInterest(object): 
   
-    def __init__(self, id, id_user, id_skill):
+	def __init__(self, id, id_user, id_skill):
 	    self.id = id
 	    self.id_user = id_user
 	    self.id_skill = id_skill
