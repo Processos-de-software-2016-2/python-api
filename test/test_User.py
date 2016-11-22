4372088de6dfbdf6e60a83c42d705a488b46acbb
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2016-11-22 17:12:51 -0300

    Corrige testes #29

--- test/test_User.py
+++ test/test_User.py
@@ -9,37 +9,58 @@ class FunTest(unittest.TestCase):
     urlbase = "http://127.0.0.1:8000"
 
     def testPost(self):
-             url= "/user"
-             param = { "name":"Teste1","email":"teste1@teste.com","age":"21","password":"123456"}
-             self.assertEqual(self.aux.postFunction(self.urlbase, url, param), 201)
-             url = "/user/email/teste1@teste.com"
-             res = self.aux.getFunction(self.urlbase, url)
-             idd = json.loads(res)[0]['id']
+        email = "apiteste1@teste.com"
 
-             url = "/user/"
-             self.aux.deleteFunction(self.urlbase, url,idd)
+        self.deleteUser(email)
+
+        url= "/user"
+        param = { "name":"Teste1","email":email,"age":"21","password":"123456"}
+        self.assertEqual(self.aux.postFunction(self.urlbase, url, param), 201)
+
+        self.deleteUser(email)
 
     def testGetByEmail(self):
-             url= "/user"
-             param = { "name":"Teste","email":"teste2@teste.com","age":"21","password":"123456"}
-             self.aux.postFunction(self.urlbase, url, param)
-             url = "/user/email/teste2@teste.com"
-             res = self.aux.getFunction(self.urlbase, url)
-             self.assertEqual(json.loads(res)[0]['email'], "teste2@teste.com")
+        email = "apiteste2@teste.com"
 
-             idd = json.loads(res)[0]['id']
-             url = "/user/"
-             self.aux.deleteFunction(self.urlbase, url,idd)
+        self.deleteUser(email)
+
+        url= "/user"
+        param = { "name":"Teste","email":email,"age":"21","password":"123456"}
+        self.aux.postFunction(self.urlbase, url, param)
+        url = "/user/email/" + email
+        res = self.aux.getFunction(self.urlbase, url)
+        self.assertEqual(json.loads(res)[0]['email'], email)
+
+        self.deleteUser(email)
 
     def testDelete(self):
-                 url= "/user"
-                 param = { "name":"Teste3","email":"teste3@teste.com","age":"21","password":"123456"}
-                 self.aux.postFunction(self.urlbase, url, param)
-                 url = "/user/email/teste3@teste.com"
-                 res = self.aux.getFunction(self.urlbase, url)
-                 idd = json.loads(res)[0]['id']
-                 url = "/user/"
-                 self.assertEqual( self.aux.deleteFunction(self.urlbase, url,idd), 200)
+        email = "apiteste3@teste.com"
+
+        self.deleteUser(email)
+
+        url= "/user"
+        param = { "name":"Teste3","email":email,"age":"21","password":"123456"}
+        self.aux.postFunction(self.urlbase, url, param)
+
+        url = "/user/email/" + email
+        res = self.aux.getFunction(self.urlbase, url)
+        idd = json.loads(res)[0]['id']
+        url = "/user/"
+        self.assertEqual( self.aux.deleteFunction(self.urlbase, url,idd), 200)
+
+    def deleteUser(self, email):
+        url = "/user/email/" + email
+        res = self.aux.getFunction(self.urlbase, url)
+
+        if res == "[]":
+            return
+            
+        idd = json.loads(res)[0]['id']
+
+        url = "/user/"
+        self.aux.deleteFunction(self.urlbase, url,idd)
+
+
 
 if __name__ == '__main__':
         unittest.main()
