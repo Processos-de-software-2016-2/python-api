da9bceb1ec9b4cdcc182448064b2895e21b4914e
Stefano Momo Loss <momoloss10@gmail.com>
2016-11-22 18:39:21 -0300

    Finalização do API User-Interest Fixes #35

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from controller.users_info import *
 from controller.image import *
 from controller.skills import *
 from controller.user_skill import *
+from controller.user_interest import *
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
@@ -31,3 +32,6 @@ app.add_route('/skill/{id}', Skill())
 app.add_route('/users/skills', UserSkill())
 app.add_route('/user/{id}/skills', UserSkill_User())
 app.add_route('/skill/{id}/users', UserSkill_Skill())
+app.add_route('/users/interests', UserInterest())
+app.add_route('/user/{id}/interests', UserInterest_User())
+app.add_route('/interest/{id}/users', UserInterest_Interest())
